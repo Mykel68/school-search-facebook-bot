467a51f93e9649183e499f60a79851e7d9eb047f
mykel68 <olowookeremicheal9568@gmail.com>
2025-03-15 14:19:37 +0100

    feat:added a new message on home api

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+// Home route: Shows a welcome message
+router.get('/', (req, res) => {
+    res.send('Welcome to the Facebook School Bot API');
+});
+
 router.use('/facebook', require('./facebook.routes'));
 
 module.exports = router;
